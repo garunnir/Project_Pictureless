4fa4ab2c7826dc2424d32fa58ca883a4645a2572
garunnir <65020562+garunnir@users.noreply.github.com>
2023-08-23 12:10:16 +0900

    bodyparts addon function
    이벤트 부분에서 메모리 누수 발생중.
    이벤트로 바꿔서 사용해보자

--- Assets/Dist/Scripts/Charactor/BodyParts.cs
+++ Assets/Dist/Scripts/Charactor/BodyParts.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -19,6 +18,7 @@ namespace Character.BodySystem
     public abstract class BodyFactory
     {
         //public abstract void build();
+        public Action<int> update;
     }
 
 
@@ -27,10 +27,12 @@ namespace Character.BodySystem
         public Humanoid()
         {
             HumanoidBody body = new HumanoidBody("head");
-            BodyParts upperbody = body.SetNext("neck").SetNext("upperBody");
+            update += body.core.update;
+            body.core.Update(update);
+            BodyParts upperbody = body.SetNext("neck").SetNext<HumanoidBody>("upperBody",new Inner.Organs.Breast());
             BodyParts lhand = upperbody.SetNext("leftshoulder").SetNext("leftupperarm").SetNext("leftelbow").SetNext("leftwrist").SetNext("lhand");
             BodyParts rhand = upperbody.SetNext("rightshoulder").SetNext("rightupperarm").SetNext("rightelbow").SetNext("rightwrist").SetNext("rhand");
-            BodyParts pelvis = upperbody.SetNext("velly").SetNext<MechBody>("waist").SetNext<HumanoidBody>("pelvis",new Womb());
+            BodyParts pelvis = upperbody.SetNext("velly").SetNext<MechBody>("waist").SetNext<HumanoidBody>("pelvis",new Inner.Organs.Womb());
             BodyParts rfoot = pelvis.SetNext("rupperleg").SetNext("rknee").SetNext("rfoot");
             BodyParts lfoot = pelvis.SetNext("lupperleg").SetNext("lknee").SetNext("lfoot");
             lhand.SetNext("lthumb", "lindexfinger", "lmiddlefinger", "lringfinger", "lpinky");
@@ -53,18 +55,57 @@ namespace Character.BodySystem
     {
         public List<BodyParts> corelist = new List<BodyParts>();
         public List<BodyParts> partslist = new List<BodyParts>();
+        public Dictionary<string,InnerParts> innerDic = new Dictionary<string, InnerParts>();
+        public Action<int> update;
         public Core() 
         {
-            
+            update += AccUpdate;
         }
         public void CoreCommend(string commend)//다른 파츠에 명령전달
         {
 
         }
+        public InnerParts FindInner(string name)
+        {
+            return innerDic[name];
+        }
+        public void Update(Action<int> timeupdate)
+        {
+            timeupdate += update;//매개변수로 들어와서 복사취급이 되는것인가..?
+        }
+        void AccUpdate(int time)
+        {
+            int atime = GetMinEventTime(time);
+            update(GetMinEventTime(atime));
+            if (atime != time) update(atime);
+        }
+        //~Core() 
+        //{
+        //    update -= AccUpdate;
+        //}
+        internal int GetMinEventTime(int time)
+        {
+            int min = int.MaxValue;
+            foreach (var item in innerDic)
+            {
+                int newv=item.Value.CheckMinEventTime(time);
+                if (newv < min)
+                {
+                    min= newv;
+                }
+            }
+            if(int.MaxValue == min)
+            {
+                Debug.LogWarning("innerDic is empty");
+                return time;
+            }
+            return min;
+        }
     }
 
     public abstract class BodyParts
     {
+        //public Action<int> update;
         public bool isCore;
         public Core core;
 
@@ -96,7 +137,7 @@ namespace Character.BodySystem
         }
         public BodyParts(string name,InnerParts inner) : this(name)
         {
-            
+            innerParts.Add(inner);
         }
         public BodyParts(string name, bool isCore = false) : this(name)
         {
@@ -125,6 +166,9 @@ namespace Character.BodySystem
             T parts = new T();
             parts.name = name;
             parts.innerParts.Add(inner);
+            inner.outerBody = parts;
+            inner.Init();
+            inner.CoreAdd(inner);
             next.Add(parts);
             parts.prev.Add(this);
             return parts;
@@ -270,118 +314,159 @@ namespace Character.BodySystem
             throw new System.NotImplementedException();
         }
     }
+
     public abstract class InnerParts
     {
+        public BodyParts outerBody;
+        public abstract void Init();
+        public abstract int CheckMinEventTime(int time);
+        abstract public void CoreAdd(InnerParts thisparts);
+
     }
     public abstract class Organ:InnerParts
     {
+        public Organ()
+        {
+        }
+        public Organ(BodyParts parts):this()
+        {
+            outerBody = parts;
+        }
+        public override void Init()
+        {
+            outerBody.core.update += sequnce;
+        }
         public Action<int> sequnce;
-    }
-    public class Womb : Organ
-    {
-        //혈류만 공급되면 작동한다. 장기 단위로 시스템 작동한다.
-        public BodyParts outerBody;
-        public float bloodLv;
-        public int level;
-        public bool isEnable;
-        public float readyFragRate;
-        public int menstruationRate = 1;
-        public bool actMenstruation;
-        public bool canfrag;
-        public float semenRate;
-        public bool isfrag;
-        public void Update(int time)//타임 단위는 1분
-        {
-            sequnce?.Invoke(time);
-            //시간을 오버하면....
-            //특정지점에서 한번 멈춰야 한다.
-            //특정시점에서 멈추고 이벤트를 진행해야 한다.
-            int minActTime = DivTime(time);
-            int remainTime = time - minActTime;
-            if (remainTime > 0)
-            {
-                ActALL(minActTime);
-                Update(remainTime);
-            }
-        }
-        public void ActALL(int time)
-        {
-            Menstruation(time);
-        }
-        public int DivTime(int time)
-        {
-            Mathf.Min(CheckMen(time), CheckMen(time), CheckMen(time));
-            return time;//최소시간
-        }
-        public int CheckMen(int time)//이걸 실행하면 오버시 최소 시간을 반환한다.
-        {
-            return Mathf.Min(time, actMenstruation ? (int)Mathf.Ceil(readyFragRate / menstruationRate * 2880) : (int)Mathf.Ceil((readyFragRate - 1) / menstruationRate * 43200));
-        }
-        public void Menstruation(int time)
-        {
-            if (actMenstruation)
-            {
-                readyFragRate -= 1f / 2880 * (time) * menstruationRate;
-                canfrag = false;
-                if (readyFragRate < 0) actMenstruation = false;
-            }
-            else
-            {
-                readyFragRate += 1f / 43200 * (time) * menstruationRate;
-                if(canfrag==false)
-                {
-                    canfrag = (readyFragRate > 0.5f && readyFragRate < 1);
-                }
-                if (readyFragRate > 1) actMenstruation = true;
-            }
-        }
-        public void Fragnant(int time)
-        {
-            if(semenRate > 0)
-            {
-                semenRate -= 1f / 5760 * time * menstruationRate;
-                if (canfrag)
-                {
-                    isfrag = true;
-                    sequnce += FragSequence;
-                }
-            }
-            else
-            {
-                if(isfrag)
-                {
-                    isfrag = false;
-                    sequnce -= FragSequence;
-                }
-                semenRate = 0;
-            }
-        }
-        public void FragSequence(int time)
-        {
-            //outerBody.core.corelist[0]
-            //breast 검색 코어한테 피드백
-        }
-    }
-    public class Breast : Organ
-    {
-        float milkRate;
-        int milkml;
-        int milkmlMax;
-        int lv;
 
-        public void Update(int time)
+    }
+    namespace Inner.Organs
+    {
+        public class Womb : Organ
         {
-            StartFillUp();
+            //혈류만 공급되면 작동한다. 장기 단위로 시스템 작동한다.
+            public float bloodLv;
+            public int level;
+            public bool isEnable;
+            public float readyFragRate;
+            public int menstruationRate = 1;
+            public bool actMenstruation;
+            public bool canfrag;
+            public float semenRate;
+            public bool isfrag;
+
+            public Womb() : base()
+            {
+                sequnce += Fnc;
+            }
+            public void Fnc(int time)//타임 단위는 1분
+            {
+                //시간을 오버하면....
+                //특정지점에서 한번 멈춰야 한다.
+                //특정시점에서 멈추고 이벤트를 진행해야 한다.
+                int minActTime = CheckMinEventTime(time);
+                int remainTime = time - minActTime;
+                if (remainTime > 0)
+                {
+                    ActALL(minActTime);
+                    Fnc(remainTime);
+                }
+            }
+            public void ActALL(int time)
+            {
+                Menstruation(time);
+            }
+            public override int CheckMinEventTime(int time)//이걸 실행하면 오버시 최소 시간을 반환한다.
+            {
+                return Mathf.Min(time, actMenstruation ? (int)Mathf.Ceil(readyFragRate / menstruationRate * 2880) : (int)Mathf.Ceil((readyFragRate - 1) / menstruationRate * 43200));
+            }
+            public void Menstruation(int time)
+            {
+                if (actMenstruation)
+                {
+                    readyFragRate -= 1f / 2880 * (time) * menstruationRate;
+                    canfrag = false;
+                    if (readyFragRate < 0) actMenstruation = false;
+                }
+                else
+                {
+                    readyFragRate += 1f / 43200 * (time) * menstruationRate;
+                    if (canfrag == false)
+                    {
+                        canfrag = (readyFragRate > 0.5f && readyFragRate < 1);
+                    }
+                    if (readyFragRate > 1) actMenstruation = true;
+                }
+            }
+            public void Fragnant(int time)
+            {
+                if (semenRate > 0)
+                {
+                    semenRate -= 1f / 5760 * time * menstruationRate;
+                    if (canfrag)
+                    {
+                        isfrag = true;
+                        sequnce += FragSequence;
+                    }
+                }
+                else
+                {
+                    if (isfrag)
+                    {
+                        isfrag = false;
+                        sequnce -= FragSequence;
+                    }
+                    semenRate = 0;
+                }
+            }
+            public void FragSequence(int time)
+            {
+                //outerBody.core.corelist[0]
+                //breast 검색 코어한테 피드백
+                //그냥 직접 명령하자......
+                //검색을 어떻게 할지 생각한다.
+                Breast breast = (Breast)outerBody.core.FindInner("breast");
+                if (breast != null)
+                {
+                    Debug.Log("~!@!");
+                    breast.lv++;
+                    breast.StartFillUp();
+                }
+            }
+
+            public override void CoreAdd(InnerParts thisparts)
+            {
+                outerBody.core.innerDic.Add(nameof(Womb), this);
+            }
         }
-        public void StartFillUp()
+        public class Breast : Organ
         {
-            sequnce += FillUp;
-        }
-        public void FillUp(int time)
-        {
-            milkRate += 0.001f;
+            float milkRate;
+            int milkml;
+            int milkmlMax;
+            public int lv;
+
+
+            public void StartFillUp()
+            {
+                sequnce += FillUp;
+            }
+            public void FillUp(int time)
+            {
+                milkRate += 0.001f;
+            }
+
+            public override int CheckMinEventTime(int time)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void CoreAdd(InnerParts thisparts)
+            {
+                outerBody.core.innerDic.Add(nameof(Breast), this);
+            }
         }
     }
+
 }
 
 
--- Assets/Dist/Scripts/Utillity/Test.cs
+++ Assets/Dist/Scripts/Utillity/Test.cs
@@ -1,3 +1,4 @@
+using Character.BodySystem;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
@@ -8,16 +9,21 @@ public class Test : MonoBehaviour
     [DllImport("Dll1")]
     public static extern double Sum(double a,double b);
     // Start is called before the first frame update
+    Character.BodySystem.Humanoid body;
+
     void Start()
     {
         //print(fnDll1());
         Debug.Log(Sum(2, 1));
-        new Character.BodySystem.Humanoid();
+        body = new Character.BodySystem.Humanoid();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            body.update(5);
+        }
     }
 }
