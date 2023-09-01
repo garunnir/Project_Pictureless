d6e77a0e628f612cb29319c144add21ce2163748
garunnir <65020562+garunnir@users.noreply.github.com>
2023-09-01 17:44:58 +0900

    add nativeGallary

--- Assets/Dist/Scripts/Android/GallaryImagePicker.cs
+++ Assets/Dist/Scripts/Android/GallaryImagePicker.cs
@@ -2,21 +2,10 @@ using UnityEngine;
 
 public class GalleryImagePicker : MonoBehaviour
 {
-    public AndroidJavaClass galleryImagePicker;
-
-    void Start()
-    {
-        // Android Java 클래스 초기화
-        galleryImagePicker = new AndroidJavaClass("com.example.GalleryImagePicker");
-
-        // 갤러리 이미지 선택 메서드 호출
-        galleryImagePicker.CallStatic("PickImage");
-    }
 
     // 갤러리에서 이미지 선택 후 호출되는 콜백 메서드
     public void OnImagePicked(string imagePath)
     {
-        Debug.Log("Selected image path: " + imagePath);
-        // imagePath를 사용하여 이미지를 처리합니다.
+
     }
 }
